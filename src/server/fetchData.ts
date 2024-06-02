7a369c02112799905c5db9375c81a88307bf5b53
audrhks29 <audrhks29@gmail.com>
2024-06-03 02:05:37 +0900

    Fix: error TS2307: Cannot find module

--- src/server/fetchData.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const fetchTeamData = async (slugId: string) => {
-  const response = await axios.get(
-    `https://site.api.espn.com/apis/site/v2/sports/soccer/${slugId}/teams`
-  );
-  return response.data.sports[0].leagues[0];
-};
