7cfbcd4afb63c8d10b2b7bb24e60b91a8b481c5d
audrhks29 <audrhks29@gmail.com>
2024-11-26 19:54:21 +0900

    Fix: Loading UI 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,14 +2,9 @@ import { memo } from "react";
 
 const Loading = memo(() => {
   return (
-    <div className="flex flex-col justify-center items-center p-10 min-h-screen">
-      <img
-        src="/images/loading/soccerBall.webp"
-        className="animate-spin h-44 w-44"
-        alt="로딩 중"
-      />
-      <span className="text-[30px]">Loading...</span>
-    </div>
+    <section className="flex flex-col justify-center items-center p-10 min-h-screen">
+      <span className="loading loading-spinner text-primary w-24"></span>
+    </section>
   );
 });
 
