f075d729709245a106d06858a45e9b64262fd8e3
audrhks29 <audrhks29@gmail.com>
2024-12-03 00:44:16 +0900

    UI: header 영역 shadcn/ui -> daisyui로 변경

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,9 @@
 import { memo } from "react";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 import getCurrentYear from "@/utils/getCurrentDate";
 
 const Header = memo(() => {
-  const location = useLocation();
   const currentYear = getCurrentYear();
   const headerArray = [
     { id: 1, text: "standings", link: `standings/eng.1/${currentYear}` },
@@ -15,7 +14,7 @@ const Header = memo(() => {
   return (
     <>
       <header className="inner">
-        <ul className="flex text-center items-center mb-9 h-24 leading-[96px] absolute top-0 z-20">
+        <ul className="navbar fixed bg-base-100 z-10">
           <Link to={"/"}>
             <li>
               <img
@@ -24,23 +23,16 @@ const Header = memo(() => {
               />
             </li>
           </Link>
-          {headerArray.map((item) => {
-            const isAttachStyle = location.pathname.includes(item.text);
-            return (
-              <Link to={`${item.link}`} key={item.id}>
-                <li className="w-40 text-[18px] uppercase hover:border-b">
-                  <span
-                    style={{
-                      borderBottom: isAttachStyle ? "1px solid #fff" : "none",
-                    }}
-                    className="p-1"
-                  >
-                    {item.text}
-                  </span>
-                </li>
-              </Link>
-            );
-          })}
+
+          {headerArray.map((item) => (
+            <Link
+              to={`${item.link}`}
+              key={item.id}
+              className="btn btn-ghost text-xl w-40 uppercase"
+            >
+              {item.text}
+            </Link>
+          ))}
         </ul>
       </header>
       <Outlet />
